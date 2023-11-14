2aabbb67f04657716b2aa17bd6ea64be564a7b57
MadsIversen96 <madsmiversen@hotmail.no>
2023-11-14 14:19:26 +0100

    Add DELETE and UPDATE handlers

--- routes/participants.js
+++ routes/participants.js
@@ -17,7 +17,8 @@ function isValidDateOfBirth(dob) {
 }
 
 
-// MAIN CRUDS for participants 
+// MAIN CRUDS for participants
+
 router.get('/', async function (req, res, next) {
   try {
     let allparticipants = await participants.list();
@@ -31,12 +32,12 @@ router.post('/add', async (req, res) => {
   try {
     const { email, firstname, lastname, dob, companyname, salary, currency, country, city } = req.body;
 
-    // Validate email format
+    // Validates email format
     if (!isValidEmail(email)) {
       return res.status(400).json({ status: 400, error: 'Invalid email format' });
     }
 
-    // Validate date of birth format
+    // Validates date of birth format
     if (!isValidDateOfBirth(dob)) {
       return res.status(400).json({ status: 400, error: 'Invalid date of birth format. Use yyyy/MM/DD' });
     }
@@ -70,23 +71,86 @@ router.post('/add', async (req, res) => {
     const workFragment = await participants.item(email).fragment('work').get();
     const homeFragment = await participants.item(email).fragment('home').get();
     console.log('Participant Data:', participantData, 'Work Data:', workFragment, 'Home Data', homeFragment);
-    res.status(201).json({ status: 201, Item: participantData, WorkFragment: workFragment, HomeFragment: homeFragment});
+    res.status(201).json({ status: 201, message: "Participant successfully created", Item: participantData, WorkFragment: workFragment, HomeFragment: homeFragment});
   } catch (error) {
     res.status(400).json({ status: 400, error: error.message });
   }
 });
 
-  router.delete('/:email', (req, res) => {
-  return 	
+router.delete('/:email', async (req, res) => {
+  try {
+    const participant = await participants.item(req.params.email).get();
+
+    // Checks if the participant exists
+    if (!participant) {
+      return res.status(404).json({ status: 404, error: "Participant not found." });
+    }
+
+    // Checks if the participant is already inactive/false
+    if (participant.props.active === false) {
+      return res.status(400).json({ status: 400, error: "Participant is already inactive." });
+    }
+
+    // Sets participant to inactive/false
+    await participants.set(req.params.email, {
+      active: false
+    });
+    
+    console.log(await participants.item(req.params.email).get())
+    res.status(201).json({ status: 201, message: "Participant was successfully deleted." });
+  } catch (error) {
+    res.status(500).json({ status: 500, error: error.message });
+  }
+});
+
+  router.put('/:email', async (req, res) => {
+    try {
+      const { email, firstname, lastname, dob, companyname, salary, currency, country, city } = req.body;
+  
+      // Validates email format
+      if (!isValidEmail(email)) {
+        return res.status(400).json({ status: 400, error: 'Invalid email format' });
+      }
+  
+      // Validates date of birth format
+      if (!isValidDateOfBirth(dob)) {
+        return res.status(400).json({ status: 400, error: 'Invalid date of birth format. Use yyyy/MM/DD' });
+      }
+  
+      // Updates the participant
+      await participants.set(email, {
+        firstname: firstname,
+        lastname: lastname,
+        dob: dob,
+        active: true,
+      });
+  
+      await participants.item(email).fragment('work').set({
+        companyname: companyname,
+        salary: salary,
+        currency: currency,
+      });
+  
+      await participants.item(email).fragment('home').set({
+        country: country,
+        city: city,
+      });
+  
+      const participantData = await participants.item(email).get();
+      const workFragment = await participants.item(email).fragment('work').get();
+      const homeFragment = await participants.item(email).fragment('home').get();
+      console.log('Participant Data:', participantData, 'Work Data:', workFragment, 'Home Data', homeFragment);
+      res.status(201).json({ status: 201, message: 'Participant successfully updated', Item: participantData, WorkFragment: workFragment, HomeFragment: homeFragment});
+    } catch (error) {
+      res.status(400).json({ status: 400, error: error.message });
+    }
   });
 
-  router.put('/:email', (req, res) => {
-  return 	
-  });
+  // GET different Participants DETAILS
 
-  // GET Participants DETAILS
+  //GETs personal details from active participants
   router.get('/details', (req, res) => {
-  return 	
+  
   });
 
   router.get('/details/deleted', (req, res) => {
